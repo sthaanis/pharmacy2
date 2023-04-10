e022964a7f27435d4e16d82ff0aa9fd561c35752
anish <anishshrestha541@gmail.com>
2023-04-10 17:47:52 +0545

    Added Category Filter

---
+++ resources/views/front/shop/brand.blade.php
@@ -0,0 +1,155 @@
+@extends('layout.frontend')
+@section('content')
+        <main class="main">
+        	<div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
+        		<div class="container">
+        			<h1 class="page-title">Grid 4 Columns<span>Shop</span></h1>
+        		</div><!-- End .container -->
+        	</div><!-- End .page-header -->
+            <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
+                <div class="container">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Shop</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Grid 4 Columns</li>
+                    </ol>
+                </div><!-- End .container -->
+            </nav><!-- End .breadcrumb-nav -->
+
+            <div class="page-content">
+                <div class="container">
+                	<div class="row">
+                		<div class="col-lg-9">
+                            <div class="products mb-3">
+                                <div class="row justify-content-center">
+                                    <div class="col-6 col-md-4 col-lg-4 col-xl-3">
+                                        <div class="product product-7 text-center">
+                                            <figure class="product-media">
+                                                <span class="product-label label-new">New</span>
+                                                <a href="product.html">
+                                                    <img src="{{asset('assets/frontend/images/products/product-4.jpg')}}" alt="Product image" class="product-image">
+                                                </a>
+
+                                                <div class="product-action">
+                                                    <a href="#" class="btn-product btn-cart"><span>View Product</span></a>
+                                                </div><!-- End .product-action -->
+                                            </figure><!-- End .product-media -->
+
+                                            <div class="product-body">
+                                                <h3 class="product-title"><a href="product.html">Brown paperbag waist pencil skirt</a></h3><!-- End .product-title -->
+                                                <div class="product-price">
+                                                    $60.00
+                                                </div><!-- End .product-price -->
+                                            </div><!-- End .product-body -->
+                                        </div><!-- End .product -->
+                                    </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
+                                </div><!-- End .row -->
+                            </div><!-- End .products -->
+
+
+                			<nav aria-label="Page navigation">
+							    <ul class="pagination justify-content-center">
+							        <li class="page-item disabled">
+							            <a class="page-link page-link-prev" href="#" aria-label="Previous" tabindex="-1" aria-disabled="true">
+							                <span aria-hidden="true"><i class="icon-long-arrow-left"></i></span>Prev
+							            </a>
+							        </li>
+							        <li class="page-item active" aria-current="page"><a class="page-link" href="#">1</a></li>
+							        <li class="page-item"><a class="page-link" href="#">2</a></li>
+							        <li class="page-item"><a class="page-link" href="#">3</a></li>
+							        <li class="page-item-total">of 6</li>
+							        <li class="page-item">
+							            <a class="page-link page-link-next" href="#" aria-label="Next">
+							                Next <span aria-hidden="true"><i class="icon-long-arrow-right"></i></span>
+							            </a>
+							        </li>
+							    </ul>
+							</nav>
+                		</div><!-- End .col-lg-9 -->
+                		<aside class="col-lg-3 order-lg-first">
+                			<div class="sidebar sidebar-shop">
+                		    	<div class="widget widget-collapsible">
+    								<h3 class="widget-title">
+									    <a data-toggle="collapse" href="#widget-1" role="button" aria-expanded="true" aria-controls="widget-1">
+									        Brand
+									    </a>
+									</h3><!-- End .widget-title -->
+
+									<div class="collapse show" id="widget-1">
+										<div class="widget-body">
+											<div class="filter-items filter-items-count">
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-1">
+														<label class="custom-control-label" for="cat-1">Dresses</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">3</span>
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-2">
+														<label class="custom-control-label" for="cat-2">T-shirts</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">0</span>
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-3">
+														<label class="custom-control-label" for="cat-3">Bags</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">4</span>
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-4">
+														<label class="custom-control-label" for="cat-4">Jackets</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">2</span>
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-5">
+														<label class="custom-control-label" for="cat-5">Shoes</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">2</span>
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-6">
+														<label class="custom-control-label" for="cat-6">Jumpers</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">1</span>
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-7">
+														<label class="custom-control-label" for="cat-7">Jeans</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">1</span>
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="cat-8">
+														<label class="custom-control-label" for="cat-8">Sportwear</label>
+													</div><!-- End .custom-checkbox -->
+													<span class="item-count">0</span>
+												</div><!-- End .filter-item -->
+											</div><!-- End .filter-items -->
+										</div><!-- End .widget-body -->
+									</div><!-- End .collapse -->
+        						</div><!-- End .widget -->
+                			</div><!-- End .sidebar sidebar-shop -->
+                		</aside><!-- End .col-lg-3 -->
+                	</div><!-- End .row -->
+                </div><!-- End .container -->
+            </div><!-- End .page-content -->
+        </main><!-- End .main -->
+
+@endsection
